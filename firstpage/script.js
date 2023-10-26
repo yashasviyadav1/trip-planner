93604bdbdf038878fc269a409ddd8a50f9e6af43
Yashasvi Yadav <124666305+yashasviyadav1@users.noreply.github.com>
2023-10-26 11:29:00 +0530

    connected frontpage to login and signup pages

---
+++ firstpage/script.js
@@ -0,0 +1,13 @@
+
+
+function transferToSignInPage(){
+    location.href = "../LoginPage/index.html";
+    // location.href="https://www.youtube.com";
+}
+
+function transferToSignUpPage(){
+    // location.href = "../SignupPage/index.html";
+    location.href = "../SignupPage/index.html";
+}
+
+
